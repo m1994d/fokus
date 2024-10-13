6ab48007991ad3ee4223d76782e8d520f037c9af
Marlon Andrés Diaz Suarez <marlondiaz2994@gmail.com>
2024-10-13 16:53:26 -0500

    updated js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const html = document.querySelector('html')
-const botonCorto = document.querySelector('.app__card-button--corto')
-const botonEnfoque = document.querySelector('.app__card-button--enfoque')
-const botonLargo = document.querySelector(' .app__card-button--largo')
+const botonCorto = document.querySelector('.app__card-button--corto');
+const botonEnfoque = document.querySelector('.app__card-button--enfoque');
+const botonLargo = document.querySelector(' .app__card-button--largo');
+const banner = document.querySelector('.app__image');
 
+//Cambio color de interfaz e imagen !
 botonCorto.addEventListener("click", () => {
-    html.setAttribute('data-contexto', 'descanso-corto')
-})
+    cambiarContecto('descanso-corto')
+});
 
 botonEnfoque.addEventListener("click", () => {
-    html.setAttribute('data-contexto', 'enfoque')
-})
+    cambiarContecto('enfoque')
+});
 
 botonLargo.addEventListener("click", () => {
-    html.setAttribute('data-contexto', 'descanso-largo')
-})
+    cambiarContecto('descanso-largo')
+});
+
+function cambiarContecto(contexto){
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagenes/${contexto}.png`)
+}
