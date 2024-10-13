b9aa082047a981f6578d6102dfb089c80ffcfb31
Marlon Andrés Diaz Suarez <marlondiaz2994@gmail.com>
2024-10-13 17:39:45 -0500

    fixing code

--- script.js
+++ script.js
@@ -4,21 +4,30 @@ const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonLargo = document.querySelector(' .app__card-button--largo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botones = document.querySelectorAll('.app__card-button');
 
 //Cambio color de interfaz e imagen !
 botonCorto.addEventListener("click", () => {
     cambiarContexto('descanso-corto')
+    botonCorto.classList.add('active')
 });
 
 botonEnfoque.addEventListener("click", () => {
     cambiarContexto('enfoque')
+    botonEnfoque.classList.add('active')
 });
 
 botonLargo.addEventListener("click", () => {
     cambiarContexto('descanso-largo')
+    botonLargo.classList.add('active')
 });
 
 function cambiarContexto(contexto) {
+
+    botones.forEach(function (contexto) {
+        contexto.classList.remove("active")
+    })
+
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagenes/${contexto}.png`)
 
