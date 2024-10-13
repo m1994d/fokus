856377759488283a8e4c3577f09709ec51ca900d
Marlon Andrés Diaz Suarez <marlondiaz2994@gmail.com>
2024-10-13 17:14:29 -0500

    Applying the DOM

--- script.js
+++ script.js
@@ -3,21 +3,39 @@ const botonCorto = document.querySelector('.app__card-button--corto');
 const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonLargo = document.querySelector(' .app__card-button--largo');
 const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 
 //Cambio color de interfaz e imagen !
 botonCorto.addEventListener("click", () => {
-    cambiarContecto('descanso-corto')
+    cambiarContexto('descanso-corto')
 });
 
 botonEnfoque.addEventListener("click", () => {
-    cambiarContecto('enfoque')
+    cambiarContexto('enfoque')
 });
 
 botonLargo.addEventListener("click", () => {
-    cambiarContecto('descanso-largo')
+    cambiarContexto('descanso-largo')
 });
 
-function cambiarContecto(contexto){
+function cambiarContexto(contexto) {
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagenes/${contexto}.png`)
+
+    switch (contexto) {
+        case "enfoque":
+            titulo.innerHTML = `Optimiza tu productividad,<br>
+                <strong class="app__title-strong">sumérgete en lo que importa.</strong>`
+
+            break;
+
+        case "descanso-corto":
+            titulo.innerHTML = `¿Qué tal tomar un respiro? <strong class="app__title-strong">¡Haz una pausa corta!</strong>`
+
+            break;
+
+        case "descanso-largo":
+            titulo.innerHTML = `Hora de volver a la superficie <strong class="app__title-strong"> Haz una pausa larga.</strong>`
+            break;
+    }
 }
